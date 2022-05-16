f0ca9a08dc16a70beae1dc607a83e923880a40b0
Minji Kim <minji_0801@naver.com>
2022-05-16 23:26:50 +0900

    Xcode 프로젝트 생성

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by 김민지 on 2022/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EggTimerUITests/EggTimerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EggTimerUITestsLaunchTests.swift
+//  EggTimerUITests
+//
+//  Created by 김민지 on 2022/05/16.
+//
+
+import XCTest
+
+class EggTimerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
