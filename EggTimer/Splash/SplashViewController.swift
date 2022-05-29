0e16b79b0fb82c18370fa3a9db0ca742a1c06d23
Minji Kim <minji_0801@naver.com>
2022-05-29 14:06:10 +0900

    Add splash screen

---
+++ EggTimer/Splash/SplashViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SplashViewController.swift
+//  EggTimer
+//
+//  Created by 김민지 on 2022/05/29.
+//
+
+import UIKit
+import WaterDrops
+
+final class SplashViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    private lazy var waterDropsView = WaterDropsView(
+        frame: view.frame,
+        direction: .up,
+        dropNum: 15,
+        color: .white,
+        minDropSize: 10,
+        maxDropSize: 30,
+        minLength: 50,
+        maxLength: view.bounds.height,
+        minDuration: 1,
+        maxDuration: 3
+    )
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        waterDropsView.addAnimation()
+        self.view.addSubview(waterDropsView)
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showAnimation()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.performSegue(withIdentifier: "presentTabBarController", sender: self)
+        }
+    }
+    
+    func showAnimation() {
+        UIView.animate(withDuration: 0.5, delay: 0) {
+            self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+        }
+        UIView.animate(withDuration: 0.5, delay: 0.5) {
+            self.imageView.transform = CGAffineTransform(rotationAngle: .pi * 2)
+        }
+    }
+}
